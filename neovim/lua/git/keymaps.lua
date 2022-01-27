10b2bf058396fd8b09bdaaedd17d0574163ffb49
Adrison Gomez <74667594+adrisongomez@users.noreply.github.com>
2022-01-27 08:23:56 -0400

    Fix git keymaps

--- neovim/lua/git/keymaps.lua
+++ neovim/lua/git/keymaps.lua
@@ -5,8 +5,8 @@ map('n', '<leader>gg', ':Git<CR>')
 map('n', '<leader>gp', ':Git push<CR>')
 map('n', '<leader>gf', ':Git push --force-with-lease<CR>')
 map('n', '<leader>gs', ':Git stash<CR>')
-map('n', '<leader>gb', ':Gitsigns blame_line')
-map('n', '<leader>gl', ':Git pull --rebase origin master')
+map('n', '<leader>gb', ':Gitsigns blame_line<CR>')
+map('n', '<leader>gl', ':Git pull --rebase origin master<CR>')
 
 
 -- Octo GitHub
