3b4debfffae80394e13da233c42dd78aa415d4ec
Adrison Gomez <agomez@resonance.nyc>
2023-04-10 11:15:37 -0400

    fix: exchange `<leader>fd` and ``<leader>fD` and move file_tree keymap into file_tree/init.lua
    Prefer `fd` than `fD`

--- neovim/lua/custom_finders/telescope/keymaps.lua
+++ neovim/lua/custom_finders/telescope/keymaps.lua
@@ -1,16 +1,3 @@
---[[
-	This file define all the custom keymaps for the telescope file finder.
-
-	These are the suggested keymaps in the telescope.nvim documentation:
-
-	- nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-	- nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-	- nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-	- nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-]]
-
--- find files in the current project open
-
 local map = require("global.functions").map
 local calling_telescope = 'require("telescope.builtin")'
 
@@ -22,19 +9,3 @@ create_keymap("n", "<Leader>ff", "find_files")
 create_keymap("n", "<Leader>fg", "live_grep")
 create_keymap("n", "<Leader>fb", "buffers")
 create_keymap("n", "<Leader>fh", "help_tags")
-
-map("n", "<Leader>fd", ":lua require('custom_finders.telescope.keymaps').open_file_browser()<CR>")
-map("n", "<Leader>fD", ":lua require('custom_finders.telescope.keymaps').open_file_browser_focus()<CR>")
-
-local M = {}
-
-M.open_file_browser = function()
-	require("nvim-tree.api").tree.toggle({ find_file = true, update_root= true, focus = true })
-end
-
-M.open_file_browser_focus = function()
-	local p = vim.fn.expand("%:p:h")
-	require("nvim-tree.api").tree.toggle({ find_file = true, path=p, focus = true })
-end
-
-return M
--- neovim/lua/file_tree/init.lua
+++ neovim/lua/file_tree/init.lua
@@ -1,5 +1,24 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-require("nvim-tree").setup()
-require("lsp-file-operations").setup()
+local map = require("global.functions").map
+
+local M = {}
+
+M.open_file_browser = function()
+	require("nvim-tree.api").tree.toggle({ find_file = true, update_root = true, focus = true })
+end
+
+M.open_file_browser_focus = function()
+	local p = vim.fn.expand("%:p:h")
+	require("nvim-tree.api").tree.toggle({ find_file = true, path = p, focus = true })
+end
+
+M.setup = function()
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
+	vim.opt.termguicolors = true
+	require("nvim-tree").setup()
+	require("lsp-file-operations").setup()
+	map("n", "<Leader>fD", ":lua require('file_tree').open_file_browser()<CR>")
+	map("n", "<Leader>fd", ":lua require('file_tree').open_file_browser_focus()<CR>")
+end
+
+return M
